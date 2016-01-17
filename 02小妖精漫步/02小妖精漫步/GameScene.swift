4814f20975682c175749bffc2d6894cf013cd8b4
4jchc <jiangjin5y24@163.com>
2016-01-17 13:05:06 +0800

    02小妖精漫步-02-添加发呆的小妖精

--- 02小妖精漫步/02小妖精漫步/GameScene.swift
+++ 02小妖精漫步/02小妖精漫步/GameScene.swift
@@ -9,14 +9,44 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var _idleFrames:NSArray?
+    var _walkFrames:NSArray?
+    /**
+     1. 添加发呆的小妖精
+     2. 点击屏幕，让小妖精转身
+     3. 让小妖精匀速走到手指点击的位置
+     1) 停止发呆（删除掉发呆的动画）
+     2) 开始走路动画
+     3) 计算行走的距离和时间
+     
+     */
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        // 1. 实例化纹理图集
+        let altas:SKTextureAtlas = SKTextureAtlas(named: "Goblin_Idle")
+        
+        // 2. 新建纹理数组
+        let arrayM:NSMutableArray = NSMutableArray(capacity: 28)
+        for var i:Int = 1; i <= 29; i++ {
+
+            let fileName:NSString = NSString(format: "goblin_idle_%04d", i)
+            let texture:SKTexture = altas.textureNamed(fileName as String)
+            arrayM.addObject(texture)
+        }
+        _idleFrames = arrayM
+        
+        // 3. 实例化小妖精精灵
+        let goblin:SKSpriteNode = SKSpriteNode(texture: _idleFrames![0] as? SKTexture)
+        goblin.position = CGPointMake(self.size.width / 2, self.size.height / 2);
+        
+        self.addChild(goblin)
+        // 4. 让小妖精发呆动画
+        let action:SKAction = SKAction.animateWithTextures(_idleFrames as! [SKTexture], timePerFrame: 0.1)
+        goblin.runAction(SKAction.repeatActionForever(action))
+    
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
